d6dcaf481891e2cb3e6c988d65965703e2a4687f
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-04-02 12:50:13 -0400

    react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 // default bootstrap
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // custom bootstrap theme
@@ -7,11 +13,20 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
