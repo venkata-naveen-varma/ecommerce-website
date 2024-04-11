19d988b4ac5f6eb494d4549e2029bf911bddb326
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-04-10 21:37:39 -0400

    FormComponent and LoginScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import ProductScreen from './screens/ProductScreen';
 import { Provider } from 'react-redux';
 import store from './store';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen/>}/>
       <Route path='/product/:id' element={<ProductScreen/>}/>
       <Route path='/cart' element={<CartScreen/>}/>
+      <Route path='/login' element={<LoginScreen/>}/>
     </Route>
   )
 );
