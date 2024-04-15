b0c7c35cad023674ef7de67ae8990760d22b894d
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-04-15 13:40:58 -0400

    create and Get orders API

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,59 @@ import Order from '../models/orderModel.js';
 // @route POST /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send('add order items');
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        shippingPrice,
+        totalPrice
+    } = req.body;
+    if(orderItems && orderItems.length === 0){
+        res.status(400);
+        throw new Error('No order items');
+    }else{
+        const order = new Order({
+            orderItems: orderItems.map((x)=>({
+                ...x,
+                product: x._id,
+                _id: undefined
+            })),
+            user:req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            shippingPrice,
+            totalPrice
+        });
+
+        const createOrder = await order.save();
+        res.status(201).json(createOrder);
+    }
 });
 
 // @desc Get loggedin user orders
 // @route GET /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-    res.send('Get my orders');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 // @desc Get order by ID
 // @route GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res) => {
-    res.send('Get order by id');
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    );
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 // @desc update order to paid
