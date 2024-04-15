7f911bfe63486a0742087c3697399e0fc94109fd
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-04-15 13:21:39 -0400

    order routes_Controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js'
+import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
