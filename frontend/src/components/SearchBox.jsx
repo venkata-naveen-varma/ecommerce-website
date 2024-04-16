b37ade5762f518d2e086dae983d37e0d7743f798
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-04-16 12:11:28 -0400

    searchbox fix

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -30,7 +30,7 @@ const SearchBox = () => {
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+      <Button type='submit' variant='outline-light' className='p-2 mx-2'>
         Search
       </Button>
     </Form>
